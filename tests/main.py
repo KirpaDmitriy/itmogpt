aabb0f695807e1057c7ed7468a05654a96979aa6
KirpaDmitriy <80625335+KirpaDmitriy@users.noreply.github.com>
2025-04-24 13:53:47 +0300

    Update main.py

--- tests/main.py
+++ tests/main.py
@@ -2,7 +2,7 @@ import pytest
 from fastapi.testclient import TestClient
 from main.runtime import app
 
-client = TestClient(app)=
+client = TestClient(app)
 
 def test_add_document():
     assert 1 == 1
