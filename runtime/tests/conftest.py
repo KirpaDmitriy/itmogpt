8426c90d4582bb955f4ae6e9e78aed2c22cc3aeb
KirpaDmitriy <80625335+KirpaDmitriy@users.noreply.github.com>
2025-05-05 22:10:01 +0300

    Update conftest.py

--- runtime/tests/conftest.py
+++ runtime/tests/conftest.py
@@ -5,11 +5,3 @@ import pytest
 
 back_dir = Path(__file__).parent.parent
 sys.path.append(str(back_dir))
-
-@pytest.fixture(autouse=True)
-def set_test_env():
-    os.environ["YA_CATALOG_ID"] = "1234"
-    os.environ["YA_API_KEY"] = "1234"
-    yield
-    os.environ.pop("YA_CATALOG_ID", None)
-    os.environ.pop("YA_API_KEY", None)
