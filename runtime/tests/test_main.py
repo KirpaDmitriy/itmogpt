a4cae93d87befc9d3c23bff44a97bedd96e741d2
KirpaDmitriy <80625335+KirpaDmitriy@users.noreply.github.com>
2025-05-05 22:14:06 +0300

    Update test_main.py

--- runtime/tests/test_main.py
+++ runtime/tests/test_main.py
@@ -15,4 +15,4 @@ def test_add_document():
     with patch('yagpt.yandex_gpt.get_async_completion', new_callable=AsyncMock) as mock_completion:
         mock_completion.return_value = "Текстик"
         response = client.get("/generate?text=Hello")
-        assert response.text == "Текстик"
+        assert response.text == '"Текстик"'
