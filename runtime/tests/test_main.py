2c809667fc4ba22ceaa2c64fe5dba606ce551d98
KirpaDmitriy <80625335+KirpaDmitriy@users.noreply.github.com>
2025-05-05 22:00:15 +0300

    Update test_main.py

--- runtime/tests/test_main.py
+++ runtime/tests/test_main.py
@@ -1,8 +1,12 @@
 import pytest
 from fastapi.testclient import TestClient
 from yagpt import app
+from unittest.mock import AsyncMock, patch
 
 client = TestClient(app)
 
 def test_add_document():
-    assert 1 == 1
+    with patch('yagpt.yandex_gpt.get_async_completion', new_callable=AsyncMock) as mock_completion:
+        mock_completion.return_value = "Текстик"
+        response = client.get("/generate?text=Hello")
+        assert response.text == "Текстик"
